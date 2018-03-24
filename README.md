b10136645aef0bee3cade50cfb7d97c304dee4d1
Mark Coombes <mark@markcoombes.ca>
2018-03-24 19:40:25 -0400

    Update firmware flashing script
    Fixes for proper RaspBee support; switched to look for firmware files locally only; add flashing info to README.md; bump script version to 0.3; closes #11.

--- README.md
+++ README.md
@@ -109,7 +109,7 @@ On a fresh install of Raspbian:
 
 ### Updating Conbee/RaspBee Firmware
 
-Firmware updates from the web UI will fail silently. Instead, an interactive utility script is provided as part of this Docker image that you can use to flash your device's firmware. To use it, follow the below instructions:
+Firmware updates from the web UI will fail silently. Instead, an interactive utility script is provided as part of this Docker image that you can use to flash your device's firmware. The script has been tested and verified to work for Conbee on amd64 Debian linux and armhf Raspbian Stretch and RaspBee on armhf Raspbian Stretch. To use it, follow the below instructions:
 
 1. Check your deCONZ container logs for the update firmware file name: type `docker logs [container name]`, and look for lines near the beginning of the log that look like this, noting the .CGF file name listed (you'll need this later):
 ```
@@ -120,12 +120,21 @@ GW firmware version shall be updated to: 0x261e0500
 
 2. `docker stop [container name]` or `docker-compose down` to stop your running deCONZ container (you must do this or the firmware update will fail).
 
-3. Invoke the firmware update script: `docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules marthoc/deconz`
+3. Invoke the firmware update script: `docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules -v /sys:/sys marthoc/deconz`
 
 4. Follow the prompts:
-- Type L to list attached devices, then type the number that corresponds to the Conbee or RaspBee device in the listing.
-- Type or paste the file name that corresponds to the file name that you found in the deCONZ container logs in step 1 (or, select a different filename, but you should have a good reason for doing this).
-- If the firmware download is OK, the MD5 signature matches, and the device number and file name look OK, type Y to start flashing!
+- Enter C for Conbee, or R for RaspBee.
+- If flashing Conbee, then enter the number that corresponds to the Conbee device in the listing.
+- Type or paste the full file name that corresponds to the file name that you found in the deCONZ container logs in step 1 (or, select a different filename, but you should have a good reason for doing this).
+- If the device/number and file name look OK, type Y to start flashing!
+
+5. Restart your deCONZ container (`docker start [container name]` or `docker-compose up`).
+
+#### Firmware Flashing Script FAQ
+
+Q: Why does the script give an error about not being able to unload modules ftdi_sio and usbserial, or that the device couldn't be rest?
+
+A: In order to flash the device, no other program or device on the system can be using these kernel modules or the device. Stop any program/container that could be using the modules or device (likely deCONZ) and then invoke the script again. If the error persists, you may need to temporarily remove other USB serial devices from the system in order allow the script to completely unload the kernel modules.
 
 ### Gotchas / Known Issues
 
--- armhf/root/firmware-update.sh
+++ armhf/root/firmware-update.sh
@@ -1,86 +1,112 @@
 #!/bin/bash
 
-VERSION=0.1
+VERSION=0.3
 
-echo "------------------------------------------------------------"
+echo "-------------------------------------------------------------------"
 echo " "
-echo "   marthoc/deconz Conbee/RaspBee Firmware Flashing Script"
+echo "       marthoc/deconz Conbee/RaspBee Firmware Flashing Script"
 echo " "
-echo "                    Version: $VERSION"
+echo "                       Version: $VERSION"
 echo " "
-echo "------------------------------------------------------------"
+echo "-------------------------------------------------------------------"
 echo " "
-echo "Enter the Conbee/RaspBee device number, or L to list devices"
-echo " "
-read -p "Device Number : " deviceNum
+read -p "Enter C for Conbee, R for RaspBee, or press Enter now to exit: " device
 
-if [ "$deviceNum" = "L" ] || [ "$deviceNum" = "l" ]; then
+if [ "$device" = "C" ] || [ "$device" = "c" ]; then
         echo " "
+        echo "Listing attached devices..."
+        echo " "
+
         /usr/bin/GCFFlasher_internal -l
+
         echo " "
-        echo "Enter the Conbee/RaspBee device number"
+        echo "Enter the Conbee device number, or press Enter now to exit."
         echo " "
         read -p "Device Number : " deviceNum
+
+        if [[ -z "${deviceNum// }" ]]; then
+                echo "Exiting..."
+                exit 1
+        fi
+elif [[ -z "${device// }" ]]; then
+        echo "Exiting..."
+        exit 1
 fi
 
-echo "------------------------------------------------------------"
+echo " "
+echo "-------------------------------------------------------------------"
 echo " "
 echo "Firmware available for flashing:"
 echo " "
 
-curl -s https://www.dresden-elektronik.de/rpi/deconz-firmware/ | grep deCONZ_Rpi_ | cut -d \> -f 7 | sed "s/...$//g" | grep -v .md5 | sort
+ls -1 /usr/share/deCONZ/firmware
+#curl -s https://www.dresden-elektronik.de/rpi/deconz-firmware/ | grep deCONZ_Rpi_ | cut -d \> -f 7 | sed "s/...$//g" | grep -v .md5 | sort
 
 echo " "
-echo "------------------------------------------------------------"
-echo " "
-echo "Enter the firmware file name from above, including extension"
+echo "Enter the firmware file name from above, including extension,"
+echo "or press Enter now to exit."
 echo " "
 
 read -p "File Name : " fileName
-
 echo " "
-echo "Downloading firmware file and MD5 signature..."
-echo " "
-echo "------------------------------------------------------------"
-
-wget -O /$fileName http://dresden-elektronik.de/rpi/deconz-firmware/$fileName
-wget -O /$fileName.md5 http://dresden-elektronik.de/rpi/deconz-firmware/$fileName.md5
-
-echo "------------------------------------------------------------"
-echo " "
-echo "Comparing firmware file with MD5 signature..."
-echo " "
-
-cd /
-md5sum -c $fileName.md5
-retVal=$?
-
-if [ $retVal != 0 ]; then
-        echo " "
-        echo "MD5 mismatch! Exiting..."
-        echo " "
+if [[ -z "${fileName// }" ]]; then
+        echo "Exiting..."
         exit 1
-else
-        echo " "
-        echo "MD5 match!"
-        echo " "
+        fi
+
+#echo " "
+#echo "Downloading firmware file and MD5 signature..."
+#echo " "
+#echo "------------------------------------------------------------"
+
+#wget -O /$fileName http://dresden-elektronik.de/rpi/deconz-firmware/$fileName
+#wget -O /$fileName.md5 http://dresden-elektronik.de/rpi/deconz-firmware/$fileName.md5
+
+#echo "------------------------------------------------------------"
+#echo " "
+#echo "Comparing firmware file with MD5 signature..."
+#echo " "
+
+#cd /
+#md5sum -c $fileName.md5
+#retVal=$?
+
+#if [ $retVal != 0 ]; then
+#        echo " "
+#        echo "MD5 mismatch! Exiting..."
+#        echo " "
+#        exit 1
+#else
+#        echo " "
+#        echo "MD5 match!"
+#        echo " "
+#fi
+
+echo "-------------------------------------------------------------------"
+echo " "
+
+if [ "$device" = "R" ] || [ "$device" = "r" ]; then
+        echo "Device: RaspBee"
+else 
+        echo "Conbee Device Number: $deviceNum"
 fi
 
-echo "------------------------------------------------------------"
-echo " "
-echo "Device Number: $deviceNum"
 echo " "
 echo "Firmware File: $fileName"
 echo " "
-echo "------------------------------------------------------------"
-echo " "
-read -p "Are the above device and firmware values correct? Y or N: " correctVal
+echo "Are the above device and firmware values correct?"
+read -p "Enter Y to proceed, any other entry to exit: " correctVal
 
 if [ "$correctVal" = "Y" ] || [ "$correctVal" = "y" ]; then
         echo " "
         echo "Flashing..."
         echo " "
-        /usr/bin/GCFFlasher_internal -d $deviceNum -f /$fileName
+        if [ "$device" = "C" ] || [ "$device" = "c" ]; then
+        /usr/bin/GCFFlasher_internal -d $deviceNum -f /usr/share/deCONZ/firmware/$fileName
+        elif [ "$device" = "R" ] || [ "$device" = "r" ]; then
+        /usr/bin/GCFFlasher_internal -f /usr/share/deCONZ/firmware/$fileName
+        fi
+        
         retVal=$?
         if [ $retVal != 0 ]; then
                 echo " "
