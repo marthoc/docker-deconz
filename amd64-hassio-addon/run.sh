6ffeda93bce1b111e61129f13677560568d940e9
Mark Coombes <mark@markcoombes.ca>
2018-09-26 11:34:56 -0400

    Add support for "--dev" deCONZ option

--- amd64-hassio-addon/run.sh
+++ amd64-hassio-addon/run.sh
@@ -9,19 +9,35 @@ HASSIO_DEBUG_ZDP="$(jq --raw-output '.debug_zdp' $CONFIG_PATH)"
 HASSIO_DEBUG_OTAU="$(jq --raw-output '.debug_otau' $CONFIG_PATH)"
 HASSIO_DECONZ_WEB_PORT="$(jq --raw-output '.web_port' $CONFIG_PATH)"
 HASSIO_DECONZ_WS_PORT="$(jq --raw-output '.websockets_port' $CONFIG_PATH)"
+HASSIO_DECONZ_DEVICE="$(jq --raw-output '.deconz_device' $CONFIG_PATH)"
 
 echo "[Hass.io] Starting deCONZ Hass.io Addon..."
 echo "[Hass.io] Current deCONZ version: $DECONZ_VERSION"
 echo "[Hass.io] Web UI port: $HASSIO_DECONZ_WEB_PORT"
 echo "[Hass.io] Websockets port: $HASSIO_DECONZ_WS_PORT"
 
-/usr/bin/deCONZ \
-    -platform minimal \
-    --auto-connect=1 \
-    --dbg-info=$HASSIO_DEBUG_INFO \
-    --dbg-aps=$HASSIO_DEBUG_APS \
-    --dbg-zcl=$HASSIO_DEBUG_ZCL \
-    --dbg-zdp=$HASSIO_DEBUG_ZDP \
-    --dbg-otau=$HASSIO_DEBUG_OTAU \
-    --http-port=$HASSIO_DECONZ_WEB_PORT \
-    --ws-port=$HASSIO_DECONZ_WS_PORT
+if [ -z "$HASSIO_DECONZ_DEVICE" ]; then
+    /usr/bin/deCONZ \
+        -platform minimal \
+        --auto-connect=1 \
+        --dbg-info=$HASSIO_DEBUG_INFO \
+        --dbg-aps=$HASSIO_DEBUG_APS \
+        --dbg-zcl=$HASSIO_DEBUG_ZCL \
+        --dbg-zdp=$HASSIO_DEBUG_ZDP \
+        --dbg-otau=$HASSIO_DEBUG_OTAU \
+        --http-port=$HASSIO_DECONZ_WEB_PORT \
+        --ws-port=$HASSIO_DECONZ_WS_PORT
+else
+    echo "[Hass.io] deCONZ device: $HASSIO_DECONZ_DEVICE"
+    /usr/bin/deCONZ \
+        -platform minimal \
+        --auto-connect=1 \
+        --dbg-info=$HASSIO_DEBUG_INFO \
+        --dbg-aps=$HASSIO_DEBUG_APS \
+        --dbg-zcl=$HASSIO_DEBUG_ZCL \
+        --dbg-zdp=$HASSIO_DEBUG_ZDP \
+        --dbg-otau=$HASSIO_DEBUG_OTAU \
+        --http-port=$HASSIO_DECONZ_WEB_PORT \
+        --ws-port=$HASSIO_DECONZ_WS_PORT \
+        --dev=$HASSIO_DECONZ_DEVICE
+fi
--- armhf/root/start.sh
+++ armhf/root/start.sh
@@ -5,13 +5,27 @@ echo "[marthoc/deconz] Current deCONZ version: $DECONZ_VERSION"
 echo "[marthoc/deconz] Web UI port: $DECONZ_WEB_PORT"
 echo "[marthoc/deconz] Websockets port: $DECONZ_WS_PORT"
 
-/usr/bin/deCONZ \
-    -platform minimal \
-    --auto-connect=1 \
-    --dbg-info=$DEBUG_INFO \
-    --dbg-aps=$DEBUG_APS \
-    --dbg-zcl=$DEBUG_ZCL \
-    --dbg-zdp=$DEBUG_ZDP \
-    --dbg-otau=$DEBUG_OTAU \
-    --http-port=$DECONZ_WEB_PORT \
-    --ws-port=$DECONZ_WS_PORT
+if [ "$DECONZ_DEVICE" != 0 ]; then
+    /usr/bin/deCONZ \
+        -platform minimal \
+        --auto-connect=1 \
+        --dbg-info=$DEBUG_INFO \
+        --dbg-aps=$DEBUG_APS \
+        --dbg-zcl=$DEBUG_ZCL \
+        --dbg-zdp=$DEBUG_ZDP \
+        --dbg-otau=$DEBUG_OTAU \
+        --http-port=$DECONZ_WEB_PORT \
+        --ws-port=$DECONZ_WS_PORT \
+        --dev=$DECONZ_DEVICE
+else
+    /usr/bin/deCONZ \
+        -platform minimal \
+        --auto-connect=1 \
+        --dbg-info=$DEBUG_INFO \
+        --dbg-aps=$DEBUG_APS \
+        --dbg-zcl=$DEBUG_ZCL \
+        --dbg-zdp=$DEBUG_ZDP \
+        --dbg-otau=$DEBUG_OTAU \
+        --http-port=$DECONZ_WEB_PORT \
+        --ws-port=$DECONZ_WS_PORT
+fi
