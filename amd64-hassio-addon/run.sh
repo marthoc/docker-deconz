e51305703ece20f1f9fba7a78493a0bf80c18f08
David K <142583+neffs@users.noreply.github.com>
2019-01-15 20:10:19 +0100

    Add VNC server support (#81)
    * Add optional VNC server option
    
    If enabled the deCONZ GUI can be accessed with a VNC client.
    
    * Cleanup VNC server session data before starting the server

--- amd64-hassio-addon/run.sh
+++ amd64-hassio-addon/run.sh
@@ -10,6 +10,19 @@ HASSIO_DEBUG_OTAU="$(jq --raw-output '.debug_otau' $CONFIG_PATH)"
 HASSIO_DECONZ_WEB_PORT="$(jq --raw-output '.web_port' $CONFIG_PATH)"
 HASSIO_DECONZ_WS_PORT="$(jq --raw-output '.websockets_port' $CONFIG_PATH)"
 HASSIO_DECONZ_DEVICE="$(jq --raw-output '.deconz_device' $CONFIG_PATH)"
+HASSIO_DECONZ_VNC_MODE=":$(jq --raw-output '.vnc.active' $CONFIG_PATH)"
+HASSIO_DECONZ_VNC_DISPLAY=":$(jq --raw-output '.vnc.port - 5900' $CONFIG_PATH)"
+HASSIO_DECONZ_VNC_PORT="$(jq --raw-output '.vnc.port' $CONFIG_PATH)"
+HASSIO_DECONZ_VNC_PASSWORD="$(jq --raw-output '.vnc.password' $CONFIG_PATH)"
+
+DECONZ_OPTS="--auto-connect=1 \
+        --dbg-info=$DEBUG_INFO \
+        --dbg-aps=$DEBUG_APS \
+        --dbg-zcl=$DEBUG_ZCL \
+        --dbg-zdp=$DEBUG_ZDP \
+        --dbg-otau=$DEBUG_OTAU \
+        --http-port=$DECONZ_WEB_PORT \
+        --ws-port=$DECONZ_WS_PORT"
 
 echo "[Hass.io] Starting deCONZ Hass.io Addon..."
 echo "[Hass.io] Current deCONZ version: $DECONZ_VERSION"
@@ -17,15 +30,25 @@ echo "[Hass.io] Web UI port: $HASSIO_DECONZ_WEB_PORT"
 echo "[Hass.io] Websockets port: $HASSIO_DECONZ_WS_PORT"
 echo "[Hass.io] deCONZ device: $HASSIO_DECONZ_DEVICE"
 
-/usr/bin/deCONZ \
-    -platform minimal \
-    --auto-connect=1 \
-    --dbg-info=$HASSIO_DEBUG_INFO \
-    --dbg-aps=$HASSIO_DEBUG_APS \
-    --dbg-zcl=$HASSIO_DEBUG_ZCL \
-    --dbg-zdp=$HASSIO_DEBUG_ZDP \
-    --dbg-otau=$HASSIO_DEBUG_OTAU \
-    --http-port=$HASSIO_DECONZ_WEB_PORT \
-    --ws-port=$HASSIO_DECONZ_WS_PORT \
-    --dev=$HASSIO_DECONZ_DEVICE
- 
+if [ "$HASSIO_DECONZ_VNC_MODE" == "true" ]; then
+  echo "[Hass.io] Starting VNC Server on Port $HASSIO_DECONZ_VNC_PORT"
+  if [ ! -e /data/.vnc ]; then
+  	mkdir /data/.vnc
+  fi
+  echo "$HASSIO_DECONZ_VNC_PASSWORD" | tigervncpasswd -f > /data/.vnc/passwd
+  chmod 600 /data/.vnc/passwd
+
+  # cleanup previous session data
+  tigervncserver -kill "$DECONZ_VNC_DISPLAY"
+
+  tigervncserver -SecurityTypes VncAuth,TLSVnc $HASSIO_DECONZ_VNC_DISPLAY
+  export DISPLAY=$HASSIO_DECONZ_VNC_DISPLAY
+else
+  echo "[Hass.io] Starting in minimal mode, without VNC GUI"
+  DECONZ_OPTS="$DECONZ_OPTS -platform minimal"
+fi
+
+export USER=root
+
+/usr/bin/deCONZ $DECONZ_OPTS 
+    
--- amd64/root/start.sh
+++ amd64/root/start.sh
@@ -5,27 +5,41 @@ echo "[marthoc/deconz] Current deCONZ version: $DECONZ_VERSION"
 echo "[marthoc/deconz] Web UI port: $DECONZ_WEB_PORT"
 echo "[marthoc/deconz] Websockets port: $DECONZ_WS_PORT"
 
-if [ "$DECONZ_DEVICE" != 0 ]; then
-    /usr/bin/deCONZ \
-        -platform minimal \
-        --auto-connect=1 \
+DECONZ_OPTS="--auto-connect=1 \
         --dbg-info=$DEBUG_INFO \
         --dbg-aps=$DEBUG_APS \
         --dbg-zcl=$DEBUG_ZCL \
         --dbg-zdp=$DEBUG_ZDP \
         --dbg-otau=$DEBUG_OTAU \
         --http-port=$DECONZ_WEB_PORT \
-        --ws-port=$DECONZ_WS_PORT \
-        --dev=$DECONZ_DEVICE
+        --ws-port=$DECONZ_WS_PORT"
+
+if [ "$DECONZ_VNC_MODE" != 0 ]; then
+  if [ $DECONZ_VNC_PORT -lt 5900 ]; then
+    echo "Invalid VNC Port. DECONZ_VNC_PORT must be larger than 5900"
+    exit 1
+  fi
+  DECONZ_VNC_DISPLAY=:$(($DECONZ_VNC_PORT - 5900))
+  echo "[marthoc/deconz] VNC Display: $DECONZ_VNC_DISPLAY on port $DECONZ_VNC_PORT"
+  
+  if [ ! -e /root/.vnc ]; then
+  	mkdir /root/.vnc
+  fi
+  echo "$DECONZ_VNC_PASSWORD" | tigervncpasswd -f > /root/.vnc/passwd
+  chmod 600 /root/.vnc/passwd
+
+  # cleanup previous session data
+  tigervncserver -kill "$DECONZ_VNC_DISPLAY"
+
+  tigervncserver -SecurityTypes VncAuth,TLSVnc "$DECONZ_VNC_DISPLAY"
+  export DISPLAY=$DECONZ_VNC_DISPLAY
 else
-    /usr/bin/deCONZ \
-        -platform minimal \
-        --auto-connect=1 \
-        --dbg-info=$DEBUG_INFO \
-        --dbg-aps=$DEBUG_APS \
-        --dbg-zcl=$DEBUG_ZCL \
-        --dbg-zdp=$DEBUG_ZDP \
-        --dbg-otau=$DEBUG_OTAU \
-        --http-port=$DECONZ_WEB_PORT \
-        --ws-port=$DECONZ_WS_PORT
+  echo "[marthoc/deconz] Starting without VNC GUI"
+  DECONZ_OPTS="$DECONZ_OPTS -platform minimal"
 fi
+
+if [ "$DECONZ_DEVICE" != 0 ]; then
+  DECONZ_OPTS="$DECONZ_OPTS --dev=$DECONZ_DEVICE"
+fi
+
+/usr/bin/deCONZ $DECONZ_OPTS
