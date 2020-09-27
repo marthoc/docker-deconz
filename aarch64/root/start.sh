c742c50f0fb6b6168eb6243f06baa99a850810d4
Annie <mingan@rutgers.se>
2020-09-27 11:22:00 +0200

    Add DECONZ_VNC_PASSWORD_FILE
    Enabling usage of docker secret

--- aarch64/root/start.sh
+++ aarch64/root/start.sh
@@ -29,9 +29,13 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
   fi
   
   # Set VNC password
-  echo "$DECONZ_VNC_PASSWORD" | tigervncpasswd -f > /root/.vnc/passwd
+  if [ "$DECONZ_VNC_PASSWORD_FILE" != 0 ]; then
+    cat "$DECONZ_VNC_PASSWORD_FILE" | tigervncpasswd -f > /root/.vnc/passwd
+  else
+    echo "$DECONZ_VNC_PASSWORD" | tigervncpasswd -f > /root/.vnc/passwd
+  fi
   chmod 600 /root/.vnc/passwd
-
+  
   # Cleanup previous VNC session data
   tigervncserver -kill "$DECONZ_VNC_DISPLAY"
 
