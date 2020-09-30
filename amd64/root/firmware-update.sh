67324a45cfcb1b19419b9b1607017d32c48f0ca5
Jo-Be-Co <Dev@JoBeCo.de>
2020-09-30 22:02:00 +0200

    Improve firmware flashing script with firmware download capability (#267)
    * Added functionality to download firmware files from the Dresden Elektronik website when they are not available on the local instance (i.e. not included with the deCONZ binary).

--- amd64/root/firmware-update.sh
+++ amd64/root/firmware-update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-VERSION=0.5
+VERSION=0.6
+FLASHER=/usr/bin/GCFFlasher_internal
+FW_PATH=/usr/share/deCONZ/firmware/
+FW_BASE=http://deconz.dresden-elektronik.de/deconz-firmware/
 
 echo "-------------------------------------------------------------------"
 echo " "
@@ -14,28 +17,27 @@ echo " "
 echo "Listing attached devices..."
 echo " "
 
-/usr/bin/GCFFlasher_internal -l
+$FLASHER -l
 
 echo " "
 echo "Enter the full device path, or press Enter now to exit."
 echo " "
 read -p "Device Path : " deviceName
-
+echo " "
 if [[ -z "${deviceName// }" ]]; then
         echo "Exiting..."
         exit 1
 fi
 
-echo " "
 echo "-------------------------------------------------------------------"
 echo " "
 echo "Firmware available for flashing:"
-echo " "
-
-ls -1 /usr/share/deCONZ/firmware
+ls -1 "$FW_PATH"
 
 echo " "
-echo "Enter the firmware file name from above, including extension,"
+echo "Enter the firmware file name from above, including extension."
+echo "Alternatively, you may enter the name of a firmware file to download"
+echo "from $FW_BASE"
 echo "or press Enter now to exit."
 echo " "
 
@@ -44,6 +46,39 @@ echo " "
 if [[ -z "${fileName// }" ]]; then
         echo "Exiting..."
         exit 1
+fi
+filePath="${FW_PATH%/}/$fileName"
+if [[ ! -f $filePath ]]; then
+        echo "File not found locally. Try to download?"
+        read -p "Enter Y to proceed, any other entry to exit: " answer
+        echo " "
+        if [[ $answer != [yY] ]]; then
+                echo "Exiting..."
+                exit 1
+        fi
+        echo "Downloading..."
+        echo " "
+        curl --fail --output "$filePath" "${FW_BASE%/}/$fileName"
+        retVal=$?
+        if [[ ! -f $filePath ]] || (( retVal != 0 )); then
+                echo " "
+                echo "Download Error! Please re-run this script..."
+                echo " "
+                [[ -f $filePath ]] && rm "$filePath"
+                exit $(( retVal == 0 ? 1 : retVal ))
+        fi
+        echo " "
+        echo "Download complete! Checking md5 checksum..."
+        md5=$(curl --fail --silent "${FW_BASE%/}/${fileName}.md542")
+        echo "${md5% *} ${filePath}" | md5sum --check
+        retVal=$?
+        echo " "
+        if (( retVal != 0 )); then
+                echo "Error comparing checksums! Please re-run this script..."
+                echo " "
+                rm "$filePath"
+                exit $retVal
+        fi
 fi
 
 echo "-------------------------------------------------------------------"
@@ -54,22 +89,21 @@ echo "Firmware File: $fileName"
 echo " "
 echo "Are the above device and firmware values correct?"
 read -p "Enter Y to proceed, any other entry to exit: " correctVal
+echo " "
 
-if [ "$correctVal" = "Y" ] || [ "$correctVal" = "y" ]; then
-        echo " "
+if [[ $correctVal == [yY] ]]; then
         echo "Flashing..."
         echo " "
-        /usr/bin/GCFFlasher_internal -d $deviceName -f /usr/share/deCONZ/firmware/$fileName
-        
+        $FLASHER -d $deviceName -f "$filePath"
+
         retVal=$?
-        if [ $retVal != 0 ]; then
+        if (( retVal != 0 )); then
                 echo " "
                 echo "Flashing Error! Please re-run this script..."
                 echo " "
                 exit $retVal
         fi
 else
-        echo " "
         echo "Exiting..."
         echo " "
         exit 1
