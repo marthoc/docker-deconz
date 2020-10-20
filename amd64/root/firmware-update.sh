c8599b99edaf152a1438fba39f8a4b6dece3c7cb
Jo-Be-Co <Dev@JoBeCo.de>
2020-10-20 23:29:09 +0200

    Streamlined exit bevahiour

--- amd64/root/firmware-update.sh
+++ amd64/root/firmware-update.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 
-VERSION=0.6
+# ===========================
+# Configuration
+# ===========================
+VERSION=0.7
 FLASHER=/usr/bin/GCFFlasher_internal
 FW_PATH=/usr/share/deCONZ/firmware/
-FW_BASE=http://deconz.dresden-elektronik.de/deconz-firmware/
+FW_ONLINE_BASE=http://deconz.dresden-elektronik.de/deconz-firmware/
+
+# ===========================
+# exit functions
+# ===========================
+# ---------------------------
+# Exit the script
+# - on exit print 1st param or "Exiting..." as message
+# - use 2nd param or 1 as exit-code
+# ---------------------------
+function exit_with_error() {
+    typeset msg="${1:-Exiting...}"
+    typeset -i retVal="${2:-1}"
+    printf "\n%s\n\n" "$msg"
+    exit $retVal
+}
+
+# ---------------------------
+# Check last return-code or 2nd param
+# - if non-zero exit with message
+# - on exit print 1st param or "Exiting..." as message
+# ---------------------------
+function exit_on_error() {
+    typeset msg="$1"
+    typeset -i retVal="${2:-$?}"
+    (( retVal == 0 )) || exit_with_error "$msg" $retVal
+}
+
+# ---------------------------
+# Check last return-code or 3rd param
+# - if non-zero exit with message
+# - on exit remove file (1st param) if present
+# - on exit print 2nd param or "Exiting..." as message
+# ---------------------------
+function delete_and_exit_on_error() {
+    typeset file="$1"
+    typeset msg="$2"
+    typeset -i retVal="${2:-$?}"
+    if (( retVal != 0 )); then
+        [[ -f $file ]] && rm "$file"
+        exit_with_error "$msg" $retVal
+    fi
+}
+
+# ---------------------------
+# Check 1st param as user-input
+# - exit script on empty input
+# ---------------------------
+function exit_on_enter() {
+    typeset input="$1"
+    [[ -n $input ]] || exit_with_error
+}
 
 echo "-------------------------------------------------------------------"
 echo " "
@@ -22,13 +76,11 @@ $FLASHER -l
 echo " "
 echo "Enter the full device path, or press Enter now to exit."
 echo " "
-read -p "Device Path : " deviceName
-echo " "
-if [[ -z "${deviceName// }" ]]; then
-        echo "Exiting..."
-        exit 1
-fi
 
+read -p "Device path : " deviceName
+exit_on_enter $deviceName
+
+echo " "
 echo "-------------------------------------------------------------------"
 echo " "
 echo "Firmware available for flashing:"
@@ -37,74 +89,44 @@ ls -1 "$FW_PATH"
 echo " "
 echo "Enter the firmware file name from above, including extension."
 echo "Alternatively, you may enter the name of a firmware file to download"
-echo "from $FW_BASE"
+echo "from $FW_ONLINE_BASE"
 echo "or press Enter now to exit."
 echo " "
 
 read -p "File Name : " fileName
+exit_on_enter $fileName
 echo " "
-if [[ -z "${fileName// }" ]]; then
-        echo "Exiting..."
-        exit 1
-fi
 filePath="${FW_PATH%/}/$fileName"
 if [[ ! -f $filePath ]]; then
-        echo "File not found locally. Try to download?"
-        read -p "Enter Y to proceed, any other entry to exit: " answer
-        echo " "
-        if [[ $answer != [yY] ]]; then
-                echo "Exiting..."
-                exit 1
-        fi
-        echo "Downloading..."
-        echo " "
-        curl --fail --output "$filePath" "${FW_BASE%/}/$fileName"
-        retVal=$?
-        if [[ ! -f $filePath ]] || (( retVal != 0 )); then
-                echo " "
-                echo "Download Error! Please re-run this script..."
-                echo " "
-                [[ -f $filePath ]] && rm "$filePath"
-                exit $(( retVal == 0 ? 1 : retVal ))
-        fi
-        echo " "
-        echo "Download complete! Checking md5 checksum..."
-        md5=$(curl --fail --silent "${FW_BASE%/}/${fileName}.md5")
-        echo "${md5% *} ${filePath}" | md5sum --check
-        retVal=$?
-        echo " "
-        if (( retVal != 0 )); then
-                echo "Error comparing checksums! Please re-run this script..."
-                echo " "
-                rm "$filePath"
-                exit $retVal
-        fi
+    echo "File not found locally. Try to download?"
+    read -p "Enter Y to proceed, any other entry to exit: " answer
+    [[ $answer == [yY] ]] || exit_with_error
+
+    echo " "
+    echo "Downloading..."
+    echo " "
+    curl --fail --output "$filePath" "${FW_ONLINE_BASE%/}/$fileName" && [[ -f $filePath ]]
+    delete_and_exit_on_error "$filePath" "Download Error! Please re-run this script..."
+    echo " "
+    echo "Download complete! Checking md5 checksum..."
+    md5=$(curl --fail --silent "${FW_ONLINE_BASE%/}/${fileName}.md5")
+    [[ -z $md5 ]] || delete_and_exit_on_error "$filePath" "Checksum file '${fileName}.md5' not found! Please re-run this script..."
+    echo "${md5% *} ${filePath}" | md5sum --check
+    delete_and_exit_on_error "$filePath" "Error comparing checksums! Please re-run this script..."
+    echo " "
 fi
 
 echo "-------------------------------------------------------------------"
 echo " "
-echo "Device: $deviceName"
-echo " "
+echo "Device ......: $deviceName"
 echo "Firmware File: $fileName"
 echo " "
 echo "Are the above device and firmware values correct?"
 read -p "Enter Y to proceed, any other entry to exit: " correctVal
+[[ $correctVal == [yY] ]] || exit_with_error
+
 echo " "
-
-if [[ $correctVal == [yY] ]]; then
-        echo "Flashing..."
-        echo " "
-        $FLASHER -t 60 -d $deviceName -f "$filePath"
-
-        retVal=$?
-        if (( retVal != 0 )); then
-                echo " "
-                echo "Flashing Error! Please re-run this script..."
-                echo " "
-                exit $retVal
-        fi
-else
-        echo "Exiting..."
-        echo " "
-        exit 1
-fi
+echo "Flashing..."
+echo " "
+$FLASHER -d $deviceName -f "$filePath"
+exit_on_error "Flashing Error! Please re-run this script..."
