ced33549cd19bf16b627389d0111a135b2bf5d2b
Philippe Delodder <philippe@delodder.be>
2021-04-05 07:35:32 +0200

    set correct path for ssl cert and fix spacing

--- armv7/root/start.sh
+++ armv7/root/start.sh
@@ -15,7 +15,7 @@ DECONZ_OPTS="--auto-connect=1 \
         --ws-port=$DECONZ_WS_PORT"
 
 if [ "$DECONZ_VNC_MODE" != 0 ]; then
-  
+
   if [ "$DECONZ_VNC_PORT" -lt 5900 ]; then
     echo "[marthoc/deconz] ERROR - VNC port must be 5900 or greater!"
     exit 1
@@ -23,7 +23,7 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
 
   DECONZ_VNC_DISPLAY=:$(($DECONZ_VNC_PORT - 5900))
   echo "[marthoc/deconz] VNC port: $DECONZ_VNC_PORT"
-  
+
   if [ ! -e /root/.vnc ]; then
     mkdir /root/.vnc
   fi
@@ -32,7 +32,7 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
   if [ "$DECONZ_VNC_PASSWORD_FILE" != 0 ] && [ -f "$DECONZ_VNC_PASSWORD_FILE" ]; then
       DECONZ_VNC_PASSWORD=$(cat $DECONZ_VNC_PASSWORD_FILE)
   fi
-  
+
   echo "$DECONZ_VNC_PASSWORD" | tigervncpasswd -f > /root/.vnc/passwd
   chmod 600 /root/.vnc/passwd
 
@@ -41,10 +41,10 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
 
   # Set VNC security
   tigervncserver -SecurityTypes VncAuth,TLSVnc "$DECONZ_VNC_DISPLAY"
-  
+
   # Export VNC display variable
   export DISPLAY=$DECONZ_VNC_DISPLAY
-  
+
   if [ "$DECONZ_NOVNC_PORT" = 0 ]; then
     echo "[marthoc/deconz] noVNC Disabled"
   else
@@ -54,20 +54,20 @@ if [ "$DECONZ_VNC_MODE" != 0 ]; then
     fi
 
     # Assert valid SSL certificate
-    NOVNC_CERT="novnc.pem"
+    NOVNC_CERT="/root/.vnc/novnc.pem"
     if [ -f "$NOVNC_CERT" ]; then
       openssl x509 -noout -in "$NOVNC_CERT" -checkend 0 > /dev/null
       if [ $? != 0 ]; then
         echo "[marthoc/deconz] The noVNC SSL certificate has expired; generating a new certificate now."
-       	rm "$NOVNC_CERT"
+        rm "$NOVNC_CERT"
       fi
     fi
     if [ ! -f "$NOVNC_CERT" ]; then
       openssl req -x509 -nodes -newkey rsa:2048 -keyout "$NOVNC_CERT" -out "$NOVNC_CERT" -days 365 -subj "/CN=deconz"
     fi
-    
+
     #Start noVNC
-    websockify -D --web=/usr/share/novnc/ --cert=/root/.vnc/novnc.pem $DECONZ_NOVNC_PORT localhost:$DECONZ_VNC_PORT
+    websockify -D --web=/usr/share/novnc/ --cert="$NOVNC_CERT" $DECONZ_NOVNC_PORT localhost:$DECONZ_VNC_PORT
     echo "[marthoc/deconz] NOVNC port: $DECONZ_NOVNC_PORT"
   fi
 
